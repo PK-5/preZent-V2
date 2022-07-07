ae0404ce1e5aeed0761fa07950a7cac297545694
PK-5 <karumalil.p@maristcollege.school.nz>
2022-07-07 12:06:25 +1200

    Prestn, Absent and Late functions

---
+++ preZent-V2.js
@@ -0,0 +1,50 @@
+//Attendance Present, Absent, Late buttons
+
+// Student 1
+
+let atdStudent1 = document.getElementById("1")
+
+function pFunction1(){
+    atdStudent1.style.background = ("red")
+}
+
+function aFunction1(){
+    atdStudent1.style.background = ("blue")
+}
+
+function lFunction1(){
+    atdStudent1.style.background = ("purple")
+}
+
+// Student 2
+
+let atdStudent2 = document.getElementById("2")
+
+function pFunction2(){
+    atdStudent2.style.background = ("red")
+}
+
+function aFunction2(){
+    atdStudent2.style.background = ("blue")
+}
+
+function lFunction2(){
+    atdStudent2.style.background = ("purple")
+}
+
+// Student 3
+
+let atdStudent3 = document.getElementById("3")
+
+function pFunction3(){
+    atdStudent3.style.background = ("red")
+}
+
+function aFunction3(){
+    atdStudent3.style.background = ("blue")
+}
+
+function lFunction3(){
+    atdStudent3.style.background = ("purple")
+}
+
